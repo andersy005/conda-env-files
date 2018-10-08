795bf3c23f4b8d35e9ef8b9f58889579a289e952
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-10-08 17:11:19 -0600

    add script to update pangeo env

---
+++ build_pangeo_env.sh
@@ -0,0 +1,4 @@
+conda env update -f ./pangeo-env.yml 
+jupyter labextension install dask-labextension
+jupyter labextension install @jupyterlab/toc
+jupyter labextension install @pyviz/jupyterlab_pyviz
